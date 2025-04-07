c939505593be6516b6e954689320a762b1e8330b
rosio <a@gmail.com>
2025-04-07 20:15:12 -0300

    added get_sell_input

--- Jaipur.py
+++ Jaipur.py
@@ -1,14 +1,16 @@
 from Jaipur.board import Board
 from Jaipur.deck import Deck
 from Jaipur.player import Player
+from Jaipur.resource import Resource
+
 
 def choose_turn():
     #dictionary of valid words to input in addition to the corresponsing number
     valid_inputs = {
-        "1": "1", "sell": "1", "sell cards": "1",
-        "2": "2", "exchange": "2", "exchange cards": "2",
-        "3": "3", "take": "3", "take one": "3", "resource": "3", "take one resource": "3",
-        "4": "4", "camels": "4","take camels" : "4", "take all camels": "4", "camel": "4"
+        "1": 1, "sell": 1, "sell cards": 1,
+        "2": 2, "exchange": 2, "exchange cards": 2,
+        "3": 3, "take": 3, "take one": 3, "resource": 3, "take one resource": 3,
+        "4": 4, "camels": 4,"take camels" : 4, "take all camels": 4, "camel": 4
     }
     while True:
         print()
@@ -20,9 +22,43 @@ def choose_turn():
                      ).strip().lower()
 
         if turn in valid_inputs:
-            return valid_inputs[turn] #returns a string with the number chosen
+            return valid_inputs[turn] #returns an int
         else:
             print('Enter a valid action')
+
+def get_sell_input():
+    user_amount = ""
+    user_type = ""
+    valid_inputs = {
+        "diamond": Resource.DIAMOND, "di": Resource.DIAMOND,
+        "gold": Resource.GOLD, "go": Resource.GOLD,
+        "silver": Resource.SILVER, "si": Resource.SILVER,
+        "cloth": Resource.CLOTH, "cl": Resource.CLOTH,
+        "spices": Resource.SPICES, "sp": Resource.SPICES,
+        "leather": Resource.LEATHER, "le": Resource.LEATHER,
+        "camel": Resource.CAMEL, "ca": Resource.CAMEL
+    }
+    while True:
+        try:
+            user_amount = int(input('Enter amount to sell '))
+            break
+        except ValueError as e:
+            print(e)
+            continue
+    while True:
+        try:
+            user_type_input = input('Enter type of resource to sell ')
+            if user_type_input in valid_inputs:
+                user_type = valid_inputs[user_type_input]
+                break
+            if user_type_input not in valid_inputs:
+                raise ValueError('Enter a valid type of resource')
+        except ValueError as e:
+            print(e)
+            continue
+
+
+    return user_type, user_amount
 
 if __name__ == '__main__':
     deck = Deck()
@@ -34,4 +70,15 @@ if __name__ == '__main__':
     board = Board(player1, player2, deck)
 
     print(board)
-    choose_turn()
+    while True:
+        chosen_turn = choose_turn()
+        if chosen_turn == 1: #sell
+            try:
+                user_type, user_amount = get_sell_input()
+                player1.sell(board, user_type, user_amount)
+                break
+            except ValueError as e:
+                print(e)
+                continue
+        #elif
+
