f375b2db0079e8a4ca2f54c95b51df1708ba93b6
rosio <a@gmail.com>
2025-04-07 18:28:12 -0300

    Added choose turn

--- Jaipur.py
+++ Jaipur.py
@@ -2,6 +2,28 @@ from Jaipur.board import Board
 from Jaipur.deck import Deck
 from Jaipur.player import Player
 
+def choose_turn():
+    #dictionary of valid words to input in addition to the corresponsing number
+    valid_inputs = {
+        "1": "1", "sell": "1", "sell cards": "1",
+        "2": "2", "exchange": "2", "exchange cards": "2",
+        "3": "3", "take": "3", "take one": "3", "resource": "3", "take one resource": "3",
+        "4": "4", "camels": "4","take camels" : "4", "take all camels": "4", "camel": "4"
+    }
+    while True:
+        print()
+        turn = input('Choose your action to play \n'
+                     '1. Sell cards\n'
+                     '2. Exchange cards\n'
+                     '3. Take one resource\n'
+                     '4. Take all camels\n'
+                     ).strip().lower()
+
+        if turn in valid_inputs:
+            return valid_inputs[turn] #returns a string with the number chosen
+        else:
+            print('Enter a valid action')
+
 if __name__ == '__main__':
     deck = Deck()
     player1 = Player('Rocio')
@@ -12,3 +34,4 @@ if __name__ == '__main__':
     board = Board(player1, player2, deck)
 
     print(board)
+    choose_turn()
